dc1635e6db91d3d4b41a0ae21a63d31742dc576f
threi085 <128632370+threi085@users.noreply.github.com>
2023-08-23 17:34:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function apagado(){
+     document.getElementById('imagem2').src='Desenho-de-Lampada.jpg';
+
+}
+
+function acesa(){
+    document.getElementById('imagem2').src='lampada-- acessa.jpg';
+
+}
